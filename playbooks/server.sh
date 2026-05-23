a6452bf34ba59774d9e6c387f3600cc8416488f4
David Valiente <dvaliente@tekne.sv>
2026-05-23 11:57:51 -0600

    Update

--- playbooks/server.sh
+++ playbooks/server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo ansible-playbook main.yml --tags os, nftables, docker, libvirt, haproxy, repotekne, gerbera --ask-vault-pass -e@vault
+# sudo ansible-playbook main.yml --tags os, nftables, docker, libvirt, haproxy, repotekne, gerbera --ask-vault-pass -e@../group_vars_all/vault
+sudo ansible-playbook main.yml --tags haproxy --ask-vault-pass -e@../group_vars_all/vault
